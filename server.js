33832595bb7e180683602ab448067b18df9a1b85
hacker <hacker@kali>
2022-03-02 23:36:32 +0000

    buy api

--- server.js
+++ server.js
@@ -37,39 +37,6 @@ const makeRequest = async(form_data) => {
         .catch(err => err.response.data)
 }
 
-// Making post to weigh bridge buy section
-app.post('/api/v1/weighBridgeBuy', (req, res) => {
-    const formData2 = new FormData();
-    let date = req.body.date;
-    let farmer = req.body.farmer;
-    let vehicalPlate = req.body.vehicalPlate;
-    let gross = req.body.gross;
-    let tare = req.body.tare;
-    let net = req.body.net;
-    let storage = req.body.storage;
-
-    formData2.append('date', date);
-    formData2.append('farmer', farmer);
-    formData2.append('vehicalPlate', vehicalPlate);
-    formData2.append('gross', gross);
-    formData2.append('tare', tare);
-    formData2.append('net', net);
-    formData2.append('storage', storage);
-
-    makeRequest(formData)
-        .then(response => res.send(response))
-        .catch(error => res.send(error))
-
-})
-
-const makeRequest = async(form_data) => {
-    return await axios.post(weighBridgeBuyURL, formData2, {
-            headers: form_data.getHeaders()
-        })
-        .then(res => res.data)
-        .catch(err => err.response.data)
-}
-
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
 })
