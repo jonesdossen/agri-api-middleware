2c6e7705ec9e454e2a05e13823c07639f56e4c3c
hacker <hacker@kali>
2022-03-02 23:30:43 +0000

    buy api

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(cors())
 
 let URL = "https://demo.agrotraderlatex.online/api/login";
 
+let weighBridgeBuyURL = "https://demo.agrotraderlatex.online/api/weigh_bridge/add_purchase";
+
 app.get('/', (req, res) => res.send("Agri API Middleware"))
 app.post('/api/v1/login', (req, res) => {
     const formData = new FormData();
@@ -29,6 +31,39 @@ app.post('/api/v1/login', (req, res) => {
 
 const makeRequest = async(form_data) => {
     return await axios.post(URL, form_data, {
+            headers: form_data.getHeaders()
+        })
+        .then(res => res.data)
+        .catch(err => err.response.data)
+}
+
+// Making post to weigh bridge buy section
+app.post('/api/v1/weighBridgeBuy', (req, res) => {
+    const formData2 = new FormData();
+    let date = req.body.date;
+    let farmer = req.body.farmer;
+    let vehicalPlate = req.body.vehicalPlate;
+    let gross = req.body.gross;
+    let tare = req.body.tare;
+    let net = req.body.net;
+    let storage = req.body.storage;
+
+    formData2.append('date', date);
+    formData2.append('farmer', farmer);
+    formData2.append('vehicalPlate', vehicalPlate);
+    formData2.append('gross', gross);
+    formData2.append('tare', tare);
+    formData2.append('net', net);
+    formData2.append('storage', storage);
+
+    makeRequest(formData)
+        .then(response => res.send(response))
+        .catch(error => res.send(error))
+
+})
+
+const makeRequest = async(form_data) => {
+    return await axios.post(weighBridgeBuyURL, formData2, {
             headers: form_data.getHeaders()
         })
         .then(res => res.data)
